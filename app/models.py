89dc9523abbcaf1923e0212ec241564679579ea9
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-19 16:56:46 -0600

    Create Opportunity table in Database.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -9,6 +10,20 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     phone_number = db.Column(db.Integer, index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    opportunities = db.relationship('Opportunity', backref='client', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.first_name)
+
+class Opportunity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    type = db.Column(db.String(120), index=True)
+    location = db.Column(db.String(120), index=True)
+    estimated_time = db.Column(db.String(120), index=True)
+    description = db.Column(db.String(140), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Opportunity {}>'.format(self.body)
