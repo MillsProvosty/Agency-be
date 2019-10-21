c79c8778300303685c6a18b70848ea0849dd3e03
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-21 17:57:44 -0600

    Create route to create new user.
    Co-Authored-By: Tay James <tayjames@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash, redirect, url_for, jsonify, request
 from app import app
+from app import db
+from api.api.errors import bad_request
 from app.forms import LoginForm
 from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -31,3 +34,20 @@ def login():
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json() or {}
+    if 'first_name' not in data or 'last_name' not in data or 'email' not in data or 'password' not in data or 'phone_number' not in data:
+        return bad_request('Error: Missing fields')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('That email is in use, please pick another.')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
